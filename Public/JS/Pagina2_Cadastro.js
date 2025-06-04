0796714e16d4a197127d9c5a035b70179822130c
LeonardoCardoso06 <leonardocardoso.rosa@outlook.com>
2025-06-04 17:20:46 -0300

    Atualizações de Interface

--- Public/JS/Pagina2_Cadastro.js
+++ Public/JS/Pagina2_Cadastro.js
@@ -63,19 +63,6 @@ document.addEventListener("DOMContentLoaded", () => {
       input.value = valor;
     }
   }
-
-  function formatarCPF(input) {
-    let valor = input.value.replace(/\D/g, "");
-    if (valor.length > 11) valor = valor.slice(0, 11);
-    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
-    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
-    valor = valor.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
-    input.value = valor;
-  }
-
-  document
-    .getElementById("cpf")
-    .addEventListener("input", (e) => formatarCPF(e.target));
   document
     .getElementById("phone")
     .addEventListener("input", (e) => formatarTelefone(e.target));
